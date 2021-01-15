064f6d07ebd859eb0643efabb872d5298d4b6996
Max Miller <maxmiller413@gmail.com>
2021-01-15 17:40:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -12,7 +12,7 @@ def find_min_in_nested_arrays(src)
         smallest_number = src[row_index][element_index]
       elsif src[row_index][element_index] < smallest_number
         smallest_number = src[row_index][element_index]
-       end 
+      end 
       element_index += 1
     end 
     new_array << smallest_number
